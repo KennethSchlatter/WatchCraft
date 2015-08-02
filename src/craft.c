5c7b4b191461e9ad08f084f59ad15bf7480db22e
kenneth.schlatter@gmail.com <kenneth.schlatter@gmail.com>
2015-08-02 14:31:12 -0500

    Alpha  0.6.0
    There's still plenty of bugs, there's a few blocks that we could add, and it's slower than we'd like. Unfortunately, if we add more blocks, it will only get slower. Also, the text is smaller than some people would like. That can be fixed in a future update. For now, it works.

--- src/craft.c
+++ src/craft.c
@@ -3,6 +3,7 @@
 #include "main.h"
 
 Window *craft_window;
+ScrollLayer *scroll;
 static GFont craft_font;
 static BitmapLayer *crafting_table;
 static GBitmap *background_bitmap;
@@ -19,80 +20,90 @@ TextLayer *key;
 
 void craft_window_load(Window *window)
 {
+	
+	scroll = scroll_layer_create(layer_get_bounds(window_get_root_layer(window)));
+	scroll_layer_set_click_config_onto_window(scroll, craft_window);
+	
   //Create GBitmap, then set to created BitmapLayer
   background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CRAFT);
   crafting_table = bitmap_layer_create(GRect(12, 12, 120, 120));
   bitmap_layer_set_bitmap(crafting_table, background_bitmap);
-  layer_add_child(window_get_root_layer(craft_window), bitmap_layer_get_layer(crafting_table));
+  scroll_layer_add_child(scroll, bitmap_layer_get_layer(crafting_table));
 
 	a1 = text_layer_create(GRect(20, 20, 32, 32));
 	text_layer_set_background_color(a1, GColorClear);
 	text_layer_set_text_color(a1, GColorBlack);
     text_layer_set_text(a1, craft_recipe.simple_table[0]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(a1));
+	scroll_layer_add_child(scroll, text_layer_get_layer(a1));
 	text_layer_set_text_alignment(a1, GTextAlignmentCenter);
 
 	b1 = text_layer_create(GRect(56, 20, 32, 32));
 	text_layer_set_background_color(b1, GColorClear);
 	text_layer_set_text_color(b1, GColorBlack);
     text_layer_set_text(b1, craft_recipe.simple_table[1]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(b1));
+	scroll_layer_add_child(scroll,  text_layer_get_layer(b1));
 	text_layer_set_text_alignment(b1, GTextAlignmentCenter);
 
 	c1 = text_layer_create(GRect(92, 20, 32, 32));
 	text_layer_set_background_color(c1, GColorClear);
 	text_layer_set_text_color(c1, GColorBlack);
     text_layer_set_text(c1, craft_recipe.simple_table[2]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(c1));
+	scroll_layer_add_child(scroll, text_layer_get_layer(c1));
 	text_layer_set_text_alignment(c1, GTextAlignmentCenter);
 
 	a2 = text_layer_create(GRect(20, 56, 32, 32));
 	text_layer_set_background_color(a2, GColorClear);
 	text_layer_set_text_color(a2, GColorBlack);
     text_layer_set_text(a2, craft_recipe.simple_table[3]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(a2));
+	 scroll_layer_add_child(scroll, text_layer_get_layer(a2));
 	text_layer_set_text_alignment(a2, GTextAlignmentCenter);
 
 	b2 = text_layer_create(GRect(56, 56, 32, 32));
 	text_layer_set_background_color(b2, GColorClear);
 	text_layer_set_text_color(b2, GColorBlack);
     text_layer_set_text(b2, craft_recipe.simple_table[4]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(b2));
+	scroll_layer_add_child(scroll, text_layer_get_layer(b2));
 	text_layer_set_text_alignment(b2, GTextAlignmentCenter);
 
 	c2 = text_layer_create(GRect(92, 56, 32, 32));
 	text_layer_set_background_color(c2, GColorClear);
 	text_layer_set_text_color(c2, GColorBlack);
     text_layer_set_text(c2, craft_recipe.simple_table[5]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(c2));
+	scroll_layer_add_child(scroll, text_layer_get_layer(c2));
 	text_layer_set_text_alignment(c2, GTextAlignmentCenter);
 
 	a3 = text_layer_create(GRect(20, 92, 32, 32));
 	text_layer_set_background_color(a3, GColorClear);
 	text_layer_set_text_color(a3, GColorBlack);
     text_layer_set_text(a3, craft_recipe.simple_table[6]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(a3));
+	scroll_layer_add_child(scroll, text_layer_get_layer(a3));
 	text_layer_set_text_alignment(a3, GTextAlignmentCenter);
 
 	b3 = text_layer_create(GRect(56, 92, 32, 32));
 	text_layer_set_background_color(b3, GColorClear);
 	text_layer_set_text_color(b3, GColorBlack);
     text_layer_set_text(b3, craft_recipe.simple_table[7]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(b3));
+	scroll_layer_add_child(scroll, text_layer_get_layer(b3));
 	text_layer_set_text_alignment(b3, GTextAlignmentCenter);
 
 	c3 = text_layer_create(GRect(92, 92, 32, 32));
 	text_layer_set_background_color(c3, GColorClear);
 	text_layer_set_text_color(c3, GColorBlack);
     text_layer_set_text(c3, craft_recipe.simple_table[8]);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(c3));
+	scroll_layer_add_child(scroll, text_layer_get_layer(c3));
 	text_layer_set_text_alignment(c3, GTextAlignmentCenter);
 
 	key = text_layer_create(GRect(0, 144, 144, 168));
 	text_layer_set_background_color(key, GColorWhite);
 	text_layer_set_text_color(key, GColorBlack);
-	layer_add_child(window_get_root_layer(craft_window), text_layer_get_layer(key));
+	static char buffer[300];
+	snprintf(buffer, sizeof(buffer), "%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s", craft_recipe.simple_table[0], craft_recipe.advanced_table[0],  craft_recipe.simple_table[1], craft_recipe.advanced_table[1], craft_recipe.simple_table[2], craft_recipe.advanced_table[2], craft_recipe.simple_table[3], craft_recipe.advanced_table[3], craft_recipe.simple_table[4], craft_recipe.advanced_table[4], craft_recipe.simple_table[5], craft_recipe.advanced_table[5], craft_recipe.simple_table[6], craft_recipe.advanced_table[6], craft_recipe.simple_table[7], craft_recipe.advanced_table[7], craft_recipe.simple_table[8], craft_recipe.advanced_table[8]);
+	scroll_layer_add_child(scroll, text_layer_get_layer(key));
+	text_layer_set_text(key, buffer);
 	text_layer_set_text_alignment(key, GTextAlignmentCenter);
+	
+	scroll_layer_set_content_size(scroll, GSize(144, 300));
+	layer_add_child(window_get_root_layer(window), scroll_layer_get_layer(scroll));
 
 return;
     //8-Bit Font
@@ -106,6 +117,8 @@ return;
     text_layer_set_font(a3, craft_font);
     text_layer_set_font(b3, craft_font);
     text_layer_set_font(c3, craft_font);
+	scroll_layer_destroy(scroll);
+	text_layer_destroy(key);
 }
 
 
